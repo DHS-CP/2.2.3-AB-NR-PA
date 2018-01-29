5f2724776bb8a6769ac071e9facfc302ac1dba3f
baakash411 <31013562+baakash411@users.noreply.github.com>
2018-01-28 21:30:59 -0800

    Update FamilyListFragment.java

--- 2.2.2CollegeApp_Soln/app/src/main/java/examples/pltw/org/collegeapp/FamilyListFragment.java
+++ 2.2.2CollegeApp_Soln/app/src/main/java/examples/pltw/org/collegeapp/FamilyListFragment.java
@@ -10,24 +10,9 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 
-public class FamilyListFragment extends ListFragment { // Step 17 implemented in line 7
-    private static final String TAG = FamilyListFragment.class.getName(); // Step 19 implemented in line 8
-    private static Family mFamily; // Step 20 implemented in line 9
+public class FamilyListFragment extends ListFragment { // step 17,18
 
-    public FamilyListFragment(){ // Step 21 implemented in lines 11-13
-        mFamily = Family.get(); // Step 22 implemented in line 12
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) { // Step 23 implemented in lines 17-23
-        super.onCreate(savedInstanceState);
-        getActivity().setTitle(R.string.family_members_title);
-
-        FamilyMemberAdapter adapter = new FamilyMemberAdapter(mFamily.getFamily());
-        setListAdapter(adapter);
-    }
-
-    private class FamilyMemberAdapter extends ArrayAdapter<FamilyMember> { // Step 24 implemented in lines 27-48
+    private class FamilyMemberAdapter extends ArrayAdapter<FamilyMember> {
         public FamilyMemberAdapter(ArrayList<FamilyMember> family) {
             super(getActivity(), 0, family);
         }
@@ -49,4 +34,21 @@ public class FamilyListFragment extends ListFragment { // Step 17 implemented in
             return convertView;
         }
     }
+
+    private static final String TAG = "FamilyListFragment"; // step 19
+    private Family mFamily; // step 20
+    public FamilyListFragment() {
+        mFamily = Family.get();
+    } // step 21
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActivity().setTitle(R.string.family_members_title);
+
+        FamilyMemberAdapter adapter = new FamilyMemberAdapter(mFamily.getFamily());
+        setListAdapter(adapter);
+    }
+
+
 }
